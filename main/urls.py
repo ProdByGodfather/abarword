60276d491cf40efcc6ac67681a537e2b74c5d8d8
ProdByGodFather <mahdighasemiabari84@gmail.com>
2023-12-08 14:05:01 +0330

    create `detail` of posts

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from main import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('posts/<slug:slug>/',views.detail,name='detail')
 ]
--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ from main import models
 # home function returned home page of abarword
 def home(request):
     # get objects from models
-    lasts = models.Posts.objects.all().order_by("-date")[:4]
+    lasts = models.Posts.objects.all().order_by("-date")[:6]
 
     # context send datas to homt.html file
     context = {
@@ -16,3 +16,9 @@ def home(request):
     return render(request, 'main/home.html',context)
 
 
+def detail(request,slug):
+    post = models.Posts.objects.get(slug = slug)
+    context = {
+        'post':post
+    }
+    return render(request,'main/detail.html',context)
